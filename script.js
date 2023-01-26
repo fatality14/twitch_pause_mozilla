1b925a9ac6c668b5635ab260aa6544bac63c65d6
fatality14 <fatality14nik@gmail.com>
2023-01-26 19:26:14 +0300

    handle page reload to set pause event

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("initial")
+
 let pbuttonLost = true;
 
 function setPauseEvent(){
@@ -8,34 +10,37 @@ function setPauseEvent(){
         pbuttonLost = true;
         console.log("no pause button on the page")
     }
-    else{
-        if(pbuttonLost){
+    else if(pbuttonLost){
             let curr = pscreen.addEventListener('click', function(e){pbutton.click()});
             console.log("pause event set");
             pbuttonLost = false;
-        }
+    }
+    else{
+        console.log("pause event is already set");
     }
 }
 
+let oldHref = document.location.href;
+const body = document.querySelector("body");
+const observer = new MutationObserver(mutations => {
+    if(oldHref != document.location.href){
+        oldHref = document.location.href;
+        setPauseEvent();
+    }
+})
 
-const observeUrlChange = () => {
-    setPauseEvent();    
+observer.observe(body, { childList: true, subtree: true });
 
-    let refList = []
-    let oldHref = document.location.href;
-    const body = document.querySelector("body");
-    const observer = new MutationObserver(mutations => {
-        if(oldHref != document.location.href){
-            refList.push(oldHref)
-            oldHref = document.location.href;
 
-            if(!refList.includes(oldHref)){
-                setPauseEvent();
-            }
-        }
-    });
+if(localStorage.getItem("twitchPauseRefreshState") == 1){
+    setPauseEvent();
+    localStorage.setItem("twitchPauseRefreshState", 0);
+}
 
-    observer.observe(body, { childList: true, subtree: true });
-};
-
-window.onload = observeUrlChange;
+window.addEventListener("load", function(){
+    setPauseEvent();
+    localStorage.setItem("twitchPauseRefreshState", 0);
+})
+window.addEventListener("unload", function(){
+    localStorage.setItem("twitchPauseRefreshState", 1);
+})
